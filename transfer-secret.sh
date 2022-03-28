3600e8c40ee5d4d195c68af20df1318130edff2d
Alan <soldier4443@gmail.com>
2022-03-28 09:34:59 +0900

    Update transfer-secret

--- transfer-secret.sh
+++ transfer-secret.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-if [ -z "$1" ] || [ -z "$2" ]; then
-    echo -e "Usage: $0 <from> <to>"
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ]; then
+    echo -e "Usage: $0 <secret-name> <from> <to>"
     exit 1
 fi
 
-FROM=$1
-TO=$2
+SECRET=$1
+FROM=$2
+TO=$3
 
-COMMAND="kubectl get secret $FROM --namespace=bser -o yaml | sed 's/namespace: .*/namespace: ${TO}/' | kubectl apply -f -"
+# Check if secret exists
+kubectl get secret $SECRET --namespace=$TO &> /dev/null
+
+if [ $? -eq 0 ]; then
+    echo "Secret already exists. Skip transfer.";
+    exit 1
+fi
+
+# Transfer secret from $FROM to $TO
+COMMAND="kubectl get secret $SECRET --namespace=$FROM -o yaml | sed 's/namespace: .*/namespace: ${TO}/' | kubectl apply -f -"
 echo $COMMAND
+eval $COMMAND
